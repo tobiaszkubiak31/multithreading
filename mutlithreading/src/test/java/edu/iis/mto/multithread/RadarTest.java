3c57f7078d8f7256851947bac8261a7f092ffe6a
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-05-25 21:08:13 +0200

    Verify the execution of method with proper arguments

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,19 +1,28 @@
 package edu.iis.mto.multithread;
 
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class RadarTest {
 
-    @Test
-    public void launchPatriotOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
-    }
+	@RepeatedTest(5)
+	public void launchPatriotOnceWhenNoticesAScudMissle() {
+		int expectedLaunchedRockets = 5;
+		PatriotBattery batteryMock = mock(PatriotBattery.class);
+		RocketStarterMechanism rocketStarterMechanism = mock(RocketStarterMechanism.class);
+		BetterRadar radar = new BetterRadar(batteryMock, expectedLaunchedRockets,
+			rocketStarterMechanism);
+		Scud enemyMissle = new Scud();
+		radar.notice(enemyMissle);
+		verify(batteryMock)
+			.launchPatriot(enemyMissle, expectedLaunchedRockets, rocketStarterMechanism);
+	}
+
+
 
 }
